87183ebb14c498d915087af2465f5e8115d834a7
IAM629 <81357143+IAM629@users.noreply.github.com>
2024-03-11 17:30:46 +0800

    update api

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ document.addEventListener("DOMContentLoaded", async function () {
     clickableArea.addEventListener("touchend", handleTouchEnd);
 
 
+    async function updateServerClickCount() {
+        try {
+            var response = await fetch('https://76cztzsamic5yd6icr7uhwil340ncfze.lambda-url.eu-north-1.on.aws/');
+            var data = await response.json();
+            document.getElementById('server_click_counter').innerText = data["count"];
+        } catch (error) {
+            console.error('Error fetching visitor count:', error);
+        }
+    }
 
     async function updateVisitorCount() {
         try {
@@ -30,14 +39,18 @@ document.addEventListener("DOMContentLoaded", async function () {
         try {
             await fetch('https://6sgej564zybhk2den2zqtdhxmi0zssli.lambda-url.eu-north-1.on.aws/', {
                 method: 'POST',
+                mode: 'no-cors',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({ hidden_click_count: hiddenClickCount }),
             });
+            localStorage.setItem("hiddenLocalClickCount",0)
+            hiddenClickCount=0;
         } catch (error) {
             console.error('Error sending hiddenClickCount:', error);
         }
+        
     }
 
     function handleMouseDown() {
@@ -65,5 +78,6 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     // Update visitor count on page load
     await updateVisitorCount();
-    //setInterval(updateHiddenVisitorCount, 5000);
+    await updateServerClickCount();
+    setInterval(updateHiddenVisitorCount, 30000);
 });
