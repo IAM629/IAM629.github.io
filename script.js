7af1cb20b7e0f067785ec1dfde2bd5275193a84b
IAM629 <81357143+IAM629@users.noreply.github.com>
2024-03-12 19:36:38 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", async function () {
-    var clickCounterContainer = document.querySelector(".click_counter");
+    var clickCounterContainer = document.getElementById("click_counter");  //queryselector by class
     var clickableArea = document.getElementById("clickableArea");
     var clickableImage = document.getElementById("clickableImage");
+    var targetDate = new Date("2018-02-17T00:00:00Z").getTime();
+    var punchaudio = new Audio("punch.mp3");
 
     // Retrieve the previous values
     var clickCount = localStorage.getItem("localclickcount") || 0;
@@ -14,6 +16,24 @@ document.addEventListener("DOMContentLoaded", async function () {
     clickableArea.addEventListener("touchstart", handleTouchStart);
     clickableArea.addEventListener("touchend", handleTouchEnd);
 
+    function playSoundEffect() {
+        punchaudio.currentTime = 0; 
+        punchaudio.play();
+    }
+
+    function daysPassed() {
+        var currentDate = new Date().getTime();
+        var timeDifference = currentDate - targetDate;
+
+        var daysPassed = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        var yearsPassed = Math.floor(daysPassed / 365);
+        var monthsPassed = Math.floor((daysPassed % 365) / 30);
+        var remainingDays = daysPassed % 30;
+
+        var daysPassedText = yearsPassed + "年" + monthsPassed + "月" + remainingDays + "日";
+
+        document.getElementById("daysPassed").innerText = daysPassedText;
+    }
 
     async function updateServerClickCount() {
         try {
@@ -29,39 +49,40 @@ document.addEventListener("DOMContentLoaded", async function () {
         try {
             var response = await fetch('https://swbce2nntivq53s6pypbx5jlcm0rxykr.lambda-url.eu-north-1.on.aws/');
             var data = await response.json();
-            document.getElementById('visitorCount').innerText = data["count"];
+            document.getElementById('visitor_counter').innerText = data["count"];
         } catch (error) {
             console.error('Error fetching visitor count:', error);
         }
     }
 
     async function updateHiddenVisitorCount(){
-        try {
-            await fetch('https://6sgej564zybhk2den2zqtdhxmi0zssli.lambda-url.eu-north-1.on.aws/', {
-                method: 'POST',
-                mode: 'no-cors',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ hidden_click_count: hiddenClickCount }),
-            });
-            localStorage.setItem("hiddenLocalClickCount",0)
-            hiddenClickCount=0;
-        } catch (error) {
-            console.error('Error sending hiddenClickCount:', error);
+        if(hiddenClickCount!=0){
+            try {
+                await fetch('https://6sgej564zybhk2den2zqtdhxmi0zssli.lambda-url.eu-north-1.on.aws/', {
+                    method: 'POST',
+                    mode: 'no-cors',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ hidden_click_count: hiddenClickCount }),
+                });
+                localStorage.setItem("hiddenLocalClickCount",0)
+                hiddenClickCount=0;
+            } catch (error) {
+                console.error('Error sending hiddenClickCount:', error);
+            }
         }
         
     }
 
     function handleMouseDown() {
-        
         clickableImage.src = "chan.png";
         clickCount++;
         hiddenClickCount++;
         localStorage.setItem("localclickcount", clickCount);
         localStorage.setItem("hiddenLocalClickCount", hiddenClickCount);
         clickCounterContainer.innerHTML = clickCount;
-        
+        playSoundEffect();
     }
 
     function handleMouseUp() {
@@ -77,6 +98,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     }
 
     // Update visitor count on page load
+    await daysPassed();
     await updateServerClickCount();
     await updateVisitorCount();
     setInterval(updateHiddenVisitorCount, 30000);
